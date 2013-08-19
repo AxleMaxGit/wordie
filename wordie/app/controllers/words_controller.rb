3624fe8d94ff16f6364ec1d84308e7d9cb30554b
AxleMaxGit <spam@thefunkyway.com>
2013-08-19 12:46:29 +1000

    after Sunday. Added Devise & Bootstrap

--- wordie/app/controllers/words_controller.rb
+++ wordie/app/controllers/words_controller.rb
@@ -1,33 +1,28 @@
 class WordsController < ApplicationController
 
-    #commented out for dev. Reinstate for release.
-    #before_filter :authenticate_user!, :only => [:new] 
+  #commented out for dev. Reinstate for release.
+  #before_filter :authenticate_user!, :only => [:new] 
 
-	def index
-		@words = Word.all
-	end
+  def index
+  	@words = Word.all
+  end
 
-	def new 
-		@word = Word.new
-	end #end new
+  def new 
+  	@word = Word.new
+  end #end new
 
-	def create
+  def create
     @word = Word.new(params.require(:word).permit(:search_term))
     @word.save_and_process
     redirect_to word_path(@word)
-	end #end create
+  end #end create
 
   def show
-    @counter = 1
+    #@counter = 1
     @word = Word.find(params[:id])
     #@results = Uri.where("word_id = ?", params[:id])
     @results = @word.uris
 
-    #code here that for_each results.id {load the paras}
-    @p_results = Paragraph.all
-
-    #you aren't passing he uri ID !!
-    #@paras = Paragraph.where("uri_id = ?", 1)
   end #end show
 
 end
--- wordie/app/models/word.rb
+++ wordie/app/models/word.rb
@@ -7,24 +7,18 @@ class Word < ActiveRecord::Base
   end
 
   def parse_para
-    puts "#######  PARSE ##############"
     #use Nokogiri to get the page_source
     page_source = Nokogiri::HTML(open(@uri.uri))
-    #Debugging text
-    puts "#######  #{@uri.to_json} ##############"
     #parse all the paragraph text that includes the search term
     @scrape_arr_para = page_source.css('p')  
     @scrape_arr_para.each do |n|
       @holder = n.text.strip
       build_regexp(self.search_term)
         if @holder =~ @regexp
-          puts @holder
           #save all of the relevant paras to the DB
           @para = Paragraph.new(:para_text => "#{@holder}", :uri_id => "#{@uri.id}")
           @para.save
-          #Debugging text in the console
-          puts @para.id
-          puts " #################################"
+          #process the text for word count
         end
       end
   end
@@ -34,7 +28,7 @@ class Word < ActiveRecord::Base
     if self.save
       @results = Google::Search::Web.new(:query => self.search_term)
       @results.each do |result|
-        if result.index < 2
+        if result.index < 3
           @uri = self.uris.new(:uri => result.uri)
           @uri.save
           self.parse_para
